5a441f0cc18bb3d46f34be4f1c390bc25778a161
jeel <jeel@3.com>
2022-12-14 09:40:19 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
-
+// function myFunction() {
+//     // var morea =document.getElementsByClassName("morea");
+//     var moreText = document.getElementById("more1");
+//     // var btnText = document.getElementById("btn1");
+    
+//     moreText.style.display="inline";
+//     // if (morea.style.display === "none") {
+//     //   btnText.innerHTML = "Read more"; 
+//     //   moreText.style.display = "none";
+//     // } else {
+//     //   btnText.innerHTML = "Read less"; 
+//     //   moreText.style.display = "inline";
+//     // }
+//   }
